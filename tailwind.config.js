f6067fff31a593628258f9721c85e8b5b15a236e
LebogangMputle <lebozikalala@gmail.com>
2024-07-30 03:25:31 +0200

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,svelte,ts}",
+    "./node_modules/flowbite-react/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
