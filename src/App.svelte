1423ea7204ff03f50af5842b7b5bcb1159df229c
LebogangMputle <lebozikalala@gmail.com>
2024-07-30 04:44:39 +0200

    "Removed Header component and its import from App.svelte, and deleted Header.svelte file."

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script>
   import { Router, Route, Link } from 'svelte-routing';
-  import Header from './lib/Header.svelte'; // Import Header component
   import Home from './pages/Home.svelte';
   import Wishlist from './pages/Wishlist.svelte';
   import Cart from './pages/Cart.svelte';
@@ -8,14 +7,7 @@
 </script>
 
 <Router>
-  <Header /> <!-- Include the Header component for consistent navigation -->
 
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/wishlist">Wishlist</Link>
-    <Link to="/cart">Cart</Link>
-    <Link to="/login">Login</Link>
-  </nav>
 
   <!-- Define routes for each page -->
   <Route path="/" component={Home} />
